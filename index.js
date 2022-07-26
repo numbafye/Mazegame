3ab515b2c6db46b968a49431f11d1c0d8622fcaf
unknown <saadiqgp@gmail.com>
2022-07-26 14:17:16 -0400

    added character avatar

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+
+// collision code BELOW
+// function detectCollision(player, obstacle) {
+//   return !(
+//     player.bottom < obstacle.top ||
+//     player.top > obstacle.bottom ||
+//     player.right < obstacle.left ||
+//     player.left > obstacle.right
+//   );
+// }
