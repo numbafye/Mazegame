48824d5dea3e4553e777f002eae7f8539abc70e3
unknown <saadiqgp@gmail.com>
2022-12-14 20:26:39 -0500

    FIXED POINTER EVENT NOT WORKING AFTER GAME RESETS

--- Maze.js
+++ Maze.js
@@ -10,6 +10,7 @@ Playbutton.addEventListener("click", () => {
   levelOne.style.display = "block";
   WINNER.style.display = "none";
   Playbutton.textContent = "PLAY";
+  Playbutton.style.display = "none";
 });
 
 // tells player they lose when they touch the border
@@ -17,11 +18,13 @@ const detectCollision = (value) => {
   if (value === "mazeborder") {
     levelOne.style.display = "none";
     levelTwo.style.display = "none";
+    Playbutton.style.display = "inline-block";
     alert("YOU FOUND TROUBLE!");
   }
   if (value === "finish") {
     Win.style.opacity = 1;
     Win.style.pointerEvents = "all";
+    Win.style.display = "inline-block";
     levelOne.style.pointerEvents = "none";
     alert("YOU ESCAPED!");
     console.log("Win");
@@ -30,6 +33,7 @@ const detectCollision = (value) => {
     alert("YOU RAN INTO TROUBLES TRAP!");
     levelOne.style.display = "none";
     levelTwo.style.display = "none";
+    Playbutton.style.display = "inline-block";
   }
   if (value === "last-finish") {
     levelOne.style.display = "none";
@@ -37,6 +41,7 @@ const detectCollision = (value) => {
     alert("YOU ESCAPED PARADISE!");
     WINNER.style.display = "block";
     Playbutton.textContent = "PLAY AGAIN";
+    Playbutton.style.display = "inline-block";
   }
 };
 // next level
@@ -44,4 +49,5 @@ Win.addEventListener("click", () => {
   levelOne.style.display = "none";
   levelTwo.style.display = "block";
   Win.style.display = "none";
+  levelOne.style.pointerEvents = "visible";
 });
